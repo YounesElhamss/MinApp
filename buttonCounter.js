2b3c55ddd6b0966344b575a3d7894ea76f82f35a
Younes Elhamss <youneselhamss@gmail.com>
2022-10-28 22:57:44 +0200

    update

--- buttonCounter.js
+++ buttonCounter.js
@@ -3,13 +3,14 @@ let reset = document.getElementById('reset');
 let numberCounter = document.getElementById('number');
 //numberCounter.innerHTML = "Antal gange:0";
 let i = 0;
+let antal = "Antal:";
 
 button.addEventListener('click', function() {
     i += 1;
-    numberCounter.innerHTML = "Antal: " + i;
+    numberCounter.innerHTML = antal + ' ' + i;
 })
 
 reset.addEventListener('click', function() {
     i = 0;
-    numberCounter.innerHTML = "Antal: " + i;
+    numberCounter.innerHTML = antal + ' ' + i;
 })
