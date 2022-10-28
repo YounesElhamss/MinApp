06b44847f0f6014fd455da7573eea2e0ac047d09
Younes Elhamss <youneselhamss@gmail.com>
2022-10-28 23:09:58 +0200

    update

--- sidenav.js
+++ sidenav.js
@@ -1,7 +1,7 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidebar").style.width = "350px";
-    document.getElementById("main").style.marginLeft = "350px";
+    document.getElementById("mySidebar").style.width = "275px";
+    document.getElementById("main").style.marginLeft = "275px";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
