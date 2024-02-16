1bad7e02646bcbf99e43e4d54946235c017a6da7
Younes Elhamss <youneselhamss@gmail.com>
2024-02-17 00:11:19 +0100

    Update buttonCounter.js

--- buttonCounter.js
+++ buttonCounter.js
@@ -1,16 +1,36 @@
-let button = document.getElementById('increment');
-let reset = document.getElementById('reset');
-let numberCounter = document.getElementById('number');
-//numberCounter.innerHTML = "Antal gange:0";
-let i = 0;
-let antal = "Antal:";
+function validateForm() {
+    let gasPrice = document.forms['form1']['gas-price'].value;
+    let kmL = document.forms['form1']['km/l'].value;
+    let commission = document.forms['form1']['commission'].value;
+    let distance = document.forms['form1']['distance'].value;
 
-button.addEventListener('click', function() {
-    i += 1;
-    numberCounter.innerHTML = antal + ' ' + i;
-})
+    let pricePerKm = gasPrice / kmL; //hvor meget har jeg så betalt for hver km jeg kan køre
+    pricePerKm = pricePerKm.toFixed(2);
 
-reset.addEventListener('click', function() {
-    i = 0;
-    numberCounter.innerHTML = antal + ' ' + i;
-})
+    let gasForDelivery = (distance * pricePerKm); //hvor meget skal jeg så betale for leveringen
+    gasForDelivery = gasForDelivery.toFixed(2);
+
+    let salaryPerKm = (commission / distance);
+    salaryPerKm = salaryPerKm.toFixed(2);
+
+    let profitPerKm = (gasForDelivery - salaryPerKm);
+    profitPerKm = profitPerKm.toFixed(2);
+
+    if (profitPerKm > pricePerKm) {
+        alert('Differencen er ' + profitPerKm + ' til din fordel.')
+    } else {
+        alert('Differencen er -' + profitPerKm);
+    }
+    
+    gasPrice = 0;
+    kmL = 0;
+    commission = 0;
+    distance = 0;
+    profitPerKm = 0;
+    pricePerKm = 0;
+    salaryPerKm = 0;
+    gasCost = 0;
+    distanceForFilledGas = 0;
+    gasForDelivery = 0;
+
+}
